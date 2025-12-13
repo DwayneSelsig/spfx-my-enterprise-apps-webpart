aa173c4e615bf8636698239431d6402b81e33b41
Dwayne Selsig <dselsig@hotmail.com>
2025-12-13 13:54:09 +0100

    Initial SPFx 1.22 solution: Entra Enterprise Apps

---
+++ src/webparts/entraEnterpriseApps/components/IEntraEnterpriseAppsProps.ts
@@ -0,0 +1,7 @@
+export interface IEntraEnterpriseAppsProps {
+  description: string;
+  isDarkTheme: boolean;
+  environmentMessage: string;
+  hasTeamsContext: boolean;
+  userDisplayName: string;
+}
